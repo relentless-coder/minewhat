dbf7c3d3fba8696b434335ca74ff823c7d1506cc
Ayush Bahuguna <lespaulayush@gmail.com>
2017-01-03 22:37:30 +0530

    Add content dynamically

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,3 +1,45 @@
-document.querySelector(".fa-times").addEventListener("click", function() {
-  document.getElementById('popup').style.display = "none";
+function get(url) {
+  var http = new XMLHttpRequest();
+  http.open("GET", url, true);
+  http.onreadystatechange = function() {
+    if(http.readyState === 4) {
+      if(http.status >= 200 && http.status <= 400) {
+        var completeData = JSON.parse(http.responseText);
+        console.log(completeData);
+        var i = 0;
+        var data = completeData.slice(i, i+2);
+        console.log(data);
+        append(data);
+        setInterval(function() {
+                  var data = completeData.slice(i, i+2);
+                  console.log(data);
+                  append(data);
+          if(i === (completeData.length - 2)) {
+              i = 0;
+          } else {
+            i += 2;
+          }
+        }, 2000);
+      }
+    }
+  };
+  http.send();
+}
+
+function append(data) {
+  var templateScript = document.getElementById('popup-product').innerHTML;
+  var theTemplate = Handlebars.compile(templateScript);
+  var compiledTemplate = theTemplate(data);
+  document.getElementById('popup').innerHTML = compiledTemplate;
+}
+
+Handlebars.registerHelper('limit_to', function(ary, max, options) {
+    if(!ary || ary.length === 0)
+        return options.inverse(this);
+
+    var result = [ ];
+    result.push(options.fn(ary[0]));
+    return result.join('');
 });
+
+get("assets/js/data.json");
